fab5492670a60dafd27d1ed9d29ab1874f8c3a42
JoshDaBosh <jwanggt@gmail.com>
2024-09-17 18:15:33 -0400

    x

---
+++ index.js
@@ -0,0 +1,24 @@
+// index.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Example route with parameters
+app.get('/hello/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}!`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
