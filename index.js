fd9fc07ec8e5622b5a7f7c697d074227b6c4167e
reiden-webber <reidenw@gmail.com>
2024-09-24 20:21:04 -0400

    Added base code to generate token

--- index.js
+++ index.js
@@ -3,9 +3,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+// get config vars
+dotenv.config();
+
+// access config var
+process.env.TOKEN_SECRET;
+
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Basic route
 app.get('/', (req, res) => {
@@ -16,6 +32,16 @@ app.get('/', (req, res) => {
 app.get('/hello/:name', (req, res) => {
   const { name } = req.params;
   res.send(`Hello, ${name}!`);
+});
+
+app.post('/api/createNewUser', (req, res) => {
+  // ...
+  console.log(req.body)
+
+  const token = generateAccessToken({ username: req.body.username });
+  res.json(token);
+
+  // ...
 });
 
 // Start the server
